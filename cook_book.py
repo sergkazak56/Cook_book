46ea4312665972265807d98d983634cf89992860
SergKazak <kazak5670@gmail.com>
2022-06-17 17:05:16 +0500

    Add merging_files.py

--- cook_book.py
+++ cook_book.py
@@ -32,38 +32,6 @@ def get_shop_list(cook_book, dishes, person_count = 1):
     :param dishes - список блюд, для которых надо составить список покупки
     :param person_count -  количество персон, для которых рассчитываются блюда (по умолчанию = 1)
     :return: shop_dict - словарь список инградиентов с необходимым количеством для закупки
-    Функция возвращает словарь shop_dict со списком инградиентов с необходимым количеством для закупки,
-    а также выводит этот список в файл для удобства распечатывания.
-    '''
-    file_path = 'files\shop_list.txt'
-    if not dishes or not cook_book:
-        return
-    shop_dict = {}
-    for dish in dishes:
-        ingradients = cook_book[dish]
-        for ingradient in ingradients:
-            if ingradient['ingredient_name'] in shop_dict:
-                shop_dict[ingradient['ingredient_name']]['quantity'] += ingradient['quantity'] * person_count
-            else:
-                amount_ingradient = {}
-                amount_ingradient['measure'] = ingradient['measure']
-                amount_ingradient['quantity'] = ingradient['quantity'] * person_count
-                shop_dict[ingradient['ingredient_name']] = amount_ingradient
-    with open(file_path, 'w', encoding='utf-8') as f_shop_list:
-        pass
-    with open(file_path, 'a', encoding='utf-8') as f_shop_list:
-        f_shop_list.write('СПИСОК ИГРАДИЕНТОВ\n\n')
-        for ingradient, amount in shop_dict.items():
-            str_to_file = f"{ingradient}: {amount['quantity']}{amount['measure']}"
-            f_shop_list.write('str_to_file\n')
-    return shop_dict
-
-def get_shop_list(cook_book, dishes, person_count = 1):
-    '''
-    :param cook_book - словарь из блюд и их инградиентов
-    :param dishes - список блюд, для которых надо составить список покупки
-    :param person_count -  количество персон, для которых рассчитываются блюда (по умолчанию = 1)
-    :return: shop_dict - словарь список инградиентов с необходимым количеством для закупки
     Функция выводит список необходимых покупок в словарь cook_book и в файл files\shop_list.txt
     для удобства распечатывания
     '''
@@ -82,10 +50,10 @@ def get_shop_list(cook_book, dishes, person_count = 1):
                 amount_ingradient['quantity'] = ingradient['quantity'] * person_count
                 shop_dict[ingradient['ingredient_name']] = amount_ingradient
     with open(file_path, 'w', encoding='utf-8') as f_shop_list:
-        f_shop_list.write(f'СПИСОК ИНГРЕДИЕНТОВ для закупки:\n{"-" * 33}\n')
+        f_shop_list.write(f'\tСПИСОК ИНГРЕДИЕНТОВ для закупки:\n\t{"-" * 33}\n')
     with open(file_path, 'a', encoding='utf-8') as f_shop_list:
         for ingradient, amount in shop_dict.items():
-            str_to_file =f"{ingradient} - {amount['quantity']} {amount['measure']}\n"
+            str_to_file =f"\t{ingradient} - {amount['quantity']} {amount['measure']}\n"
             f_shop_list.write(str_to_file)
     return shop_dict
 
